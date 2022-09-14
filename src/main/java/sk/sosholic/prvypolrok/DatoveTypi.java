55c3f452b6f51890c89d472859b1487bf5a23ba6
Tomas Kvackaj <tomas.kvackaj@sosholic.sk>
2022-09-14 11:12:54 +0200

    Initial commit

---
+++ src/main/java/sk/sosholic/prvypolrok/DatoveTypi.java
@@ -0,0 +1,24 @@
+package sk.sosholic.prvypolrok;
+
+public class DatoveTypi {
+    public static void main(String[] args) {
+        byte x = 8;
+        short c = 16;
+        int yc = 32;
+        long l = 64;
+        float cff = 32;
+        double fllflf = 64;
+        char fffl = 'f';
+        boolean lll = true;
+
+        System.out.print(x+"b ");
+        System.out.print(c+"b ");
+        System.out.print(yc+"b ");
+        System.out.print(l+"b ");
+        System.out.print(cff+"b ");
+        System.out.print(fllflf+"b ");
+        System.out.print(fffl+" ");
+        System.out.print(lll+" ");
+
+    }
+}
---
+++ src/main/java/sk/sosholic/prvypolrok/Opakovanie.java
@@ -0,0 +1,7 @@
+package sk.sosholic.prvypolrok;
+
+public class Opakovanie {
+    public static void main(String[] args) {
+        System.out.printf("Jupiiiiiiiiiiiii");
+    }
+}
